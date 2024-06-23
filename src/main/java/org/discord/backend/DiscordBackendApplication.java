744c15392215260026cc118af0a4229c595fa866
Sujan-Haldar <hsujan122@gmail.com>
2024-06-23 12:26:20 +0530

    Initial setup

---
+++ src/main/java/org/discord/backend/DiscordBackendApplication.java
@@ -0,0 +1,13 @@
+package org.discord.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/discord/backend/DiscordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.discord.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
